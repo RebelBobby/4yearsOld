72e53ef3073394dd2acbe591e78684bfaabd26da
RebelBobby <joseph.matulewicz@gmail.com>
2022-07-20 15:34:11 -0700

    4 years old

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("=> String interpolation.");
+        int age = 4;
+        string name = "Joe Matulewicz";
+
+        string greeting = string.Format("Hello {0} you are {1} years old", name, age);
+
+        Console.WriteLine(greeting);
+
+        string greeting1 = $"Hellow {name} you are {age} years old.";
+        Console.WriteLine(greeting1);
+    }
+}
